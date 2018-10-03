ea4f37fa3ab412fd93aed8ecbcf47c707c09f2d7
Maria <maria.clr.dias@gmail.com>
2018-10-02 21:50:57 -0300

    solving issue#2

---
+++ CodeFights/Arcade/Intro/add/java/add.java
@@ -0,0 +1,10 @@
+class add{
+
+  public static int add(int param1, int param2) {
+    return param1 + param2;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(add(1,2));
+  }
+}
